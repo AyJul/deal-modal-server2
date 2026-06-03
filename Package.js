5d0c65350bb02073f59df93a8dfa512e79a75b87
AyJul <jgarcia@silvercareinsgrp.com>
2026-06-03 14:00:30 -0400

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,14 @@
+{
+  "name": "deal-modal-server",
+  "version": "1.0.0",
+  "description": "Slack modal server for /deal slash command",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.0",
+    "qs": "^6.11.0"
+  }
+}
